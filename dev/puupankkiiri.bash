55e7d390e3f5a942a971e81e1a0308a4a8fa2e5d
Flammie Pirinen <flammie@iki.fi>
2018-12-14 11:14:41 +0100

    dev scrript for trees

--- dev/puupankkiiri.bash
+++ dev/puupankkiiri.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 LEGPARSER=$HOME/github/flammie/leg-ape-ud/legproc.py
-cat $@ | egrep -v '^#!' | apertium -d . krl-morph |\
-    python $LEGPARSER --not-rules apertium-krl.krl.leg
+file_id_diz=$(basename $@ | sed -e 's/.text//')
+cat $@ | egrep -v '^#!' | apertium -d . krl-morph > $file_id_diz
+python $LEGPARSER --not-rules apertium-krl.krl.leg -i $file_id_diz
