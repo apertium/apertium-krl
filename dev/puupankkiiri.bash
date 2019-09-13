849156b3f6a856a678cb7fbe5e51a1ee7fc1571c
Flammie Pirinen <flammie@iki.fi>
2019-09-13 15:47:27 +0200

    use ranked ambig parsing

--- dev/puupankkiiri.bash
+++ dev/puupankkiiri.bash
@@ -3,4 +3,4 @@
 LEGPARSER=$HOME/github/flammie/leg-ape-ud/legproc.py
 file_id_diz=$(basename $@ | sed -e 's/.text//')
 cat $@ | egrep -v '^#!' | apertium -d . krl-morph > $file_id_diz
-python $LEGPARSER --not-rules apertium-krl.krl.leg -i $file_id_diz
+python $LEGPARSER --debug --not-rules apertium-krl.krl.leg -i $file_id_diz
